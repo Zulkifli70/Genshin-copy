971e5842df11569f27fa116ad2b2916196036f3d
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-17 12:30:27 +0700

    done make navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { FaCircleUser } from "react-icons/fa6";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left-section">
+        <a href="#">
+          <img src="/genshin-logo.png" alt="genshin-logo" />
+        </a>
+
+        <a href="#" className="navbar-button">
+          HOME
+        </a>
+        <a href="#" className="navbar-button">
+          NEWS
+        </a>
+        <a href="#" className="navbar-button">
+          CHARACTER
+        </a>
+        <a href="#" className="navbar-button">
+          EXPLORE
+        </a>
+        <a href="#" className="navbar-button">
+          HoYoLAB
+        </a>
+        <a href="#" className="navbar-button">
+          TOP-UP
+        </a>
+        <a href="#" className="navbar-button">
+          REDEEM CODE
+        </a>
+      </div>
+      <div className="right-section">
+        <button className="flex items-center gap-3">
+          Log In <FaCircleUser size={25} />
+        </button>
+        <button className="p-4 bg-[#FFD005] text-[#5d4a22]">Download</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
