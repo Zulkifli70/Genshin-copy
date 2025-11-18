98e773a2a838a3de7efac979dde4c796dc38a5f3
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-18 20:51:21 +0700

    done make news list

---
+++ src/constant/index.js
@@ -0,0 +1,24 @@
+const newsList = [
+  {
+    newsTitle: "Genshin event 1",
+    newsDate: "Nov 19, 2025",
+  },
+  {
+    newsTitle: "Genshin event 2",
+    newsDate: "Nov 19, 2025",
+  },
+  {
+    newsTitle: "Genshin event 3",
+    newsDate: "Nov 19, 2025",
+  },
+  {
+    newsTitle: "Genshin event 4",
+    newsDate: "Nov 19, 2025",
+  },
+  {
+    newsTitle: "Genshin event 5",
+    newsDate: "Nov 19, 2025",
+  },
+];
+
+export { newsList };
--- src/sections/News.jsx
+++ src/sections/News.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { newsList } from "../constant";
+import { CiSquarePlus } from "react-icons/ci";
 
 const News = () => {
   return (
@@ -11,7 +13,27 @@ const News = () => {
           <img src="/event1.jpg" alt="event 1 lamia" />
         </div>
         <div className="news-list">
-          <h2>Latest</h2>
+          <div className="p-5">
+            <div className="border-b-4 border-[rgba(241,190,50,0.14)]">
+              <h2 className="border-b-4 w-fit mb-[-4px] border-[rgba(241,190,50,0.79)] text-xl py-3 px-4 text-[rgba(241,190,50,0.79)]">
+                Latest
+              </h2>
+            </div>
+            <div className="news">
+              {newsList.map((news) => (
+                <div className="news-link">
+                  <a href="#">{news.newsTitle}</a>
+                  <p>{news.newsDate}</p>
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-end mr-5 mt-6 items-center text-white">
+              <a className="flex items-center gap-2 rounded-2xl hover:text-[rgba(241,190,50,0.79)]  cursor-pointer">
+                <CiSquarePlus size={30} color="rgba(241,190,50,0.79)" />
+                More
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
