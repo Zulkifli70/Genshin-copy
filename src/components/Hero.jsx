7cb4c3bd9e0aa454b34c3daa153889b54e40713d
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-18 10:38:42 +0700

    make all platform button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,6 +21,24 @@ const Hero = () => {
         <h1 className="tagline">
           Released on Multiple Platforms - Download Now!
         </h1>
+        <div className="available-platform">
+          <div className="row-start-1 row-end-3 flex">
+            <img className="h-[120px]" src="/qrcode.png" alt="qr" />
+          </div>
+          <div className="col-start-2 col-end-4 flex gap-4">
+            <img className="w-[150px]" src="/ps5.png" alt="ps5" />
+            <img className="w-[150px]" src="/apple.png" alt="apple" />
+            <img className="w-[150px]" src="/hoyoplay-pc.png" alt="pc" />
+          </div>
+          <div className="col-start-2 col-end-4 flex gap-4">
+            <img className="w-[150px]" src="/gplay.png" alt="google play" />
+            <img className="w-[150px]" src="/xbox.png" alt="xbox" />
+            <img className="w-[150px]" src="/epic.png" alt="epic game store" />
+          </div>
+          <div className="row-start-1 row-end-3 col-start-4 col-end-5 h-full flex">
+            <img className="h-[120px]" src="/teen.png" alt="teen" />
+          </div>
+        </div>
       </div>
     </main>
   );
