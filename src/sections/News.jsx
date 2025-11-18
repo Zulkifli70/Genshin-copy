6fdf98aa89c2f3209635842937cb34ed41d5e1fd
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-18 15:05:35 +0700

    make news card skeleton and hard code

--- src/sections/News.jsx
+++ src/sections/News.jsx
@@ -6,6 +6,14 @@ const News = () => {
       <div className="news-header">
         <h1 className="text-5xl text-white font-semibold mt-45">NEWS</h1>
       </div>
+      <div className="news-card">
+        <div className="news-img">
+          <img src="/event1.jpg" alt="event 1 lamia" />
+        </div>
+        <div className="news-list">
+          <h2>Latest</h2>
+        </div>
+      </div>
     </div>
   );
 };
