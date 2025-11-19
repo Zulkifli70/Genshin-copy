bfe6e1219881abe1b8be30849fb498b152951cab
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-19 11:17:40 +0700

    Make Mondstadt region div

--- src/sections/Region.jsx
+++ src/sections/Region.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Region = () => {
-  return <div className="region-section">Region</div>;
+  return (
+    <div className="region-section">
+      <div className="region mondo">
+        <h1>Mondstadt</h1>
+      </div>
+      <div className="region liyue">Liyue</div>
+      <div className="region inazuma">Inazuma</div>
+      <div className="region sumeru">Sumeru</div>
+      <div className="region fontaine">Fontaine</div>
+      <div className="region natlan">Natlan</div>
+      <div className="region nod-krai">Nod Krai</div>
+      <div className="region-asap">Coming Soon</div>
+    </div>
+  );
 };
 
 export default Region;
