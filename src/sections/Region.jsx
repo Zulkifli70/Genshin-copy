881d07e4b9f21f2ff4e5ba449f661715ab4efbb4
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-19 13:44:57 +0700

    add character portrait

--- src/sections/Region.jsx
+++ src/sections/Region.jsx
@@ -4,14 +4,27 @@ const Region = () => {
   return (
     <div className="region-section">
       <div className="region mondo">
-        <h1 className="relative z-10">Mondstadt</h1>
+        <h1 className="relative z-10 region-name">Mondstadt</h1>
+        <img src="/chara/Jean.png" alt="Jean" className="region-chara" />
       </div>
-      <div className="region liyue">Liyue</div>
-      <div className="region inazuma">Inazuma</div>
-      <div className="region sumeru">Sumeru</div>
-      <div className="region fontaine">Fontaine</div>
-      <div className="region natlan">Natlan</div>
-      <div className="region nod-krai">Nod Krai</div>
+      <div className="region liyue">
+        <h1 className="relative z-10 region-name">Liyue</h1>
+      </div>
+      <div className="region inazuma">
+        <h1 className="relative z-10 region-name">Inazuma</h1>
+      </div>
+      <div className="region sumeru">
+        <h1 className="relative z-10 region-name">Sumeru</h1>
+      </div>
+      <div className="region fontaine">
+        <h1 className="relative z-10 region-name">Fontaine</h1>
+      </div>
+      <div className="region natlan">
+        <h1 className="relative z-10 region-name">Natlan</h1>
+      </div>
+      <div className="region nod-krai">
+        <h1 className="relative z-10 region-name">Nod Krai</h1>
+      </div>
       <div className="region asap">Coming Soon</div>
     </div>
   );
