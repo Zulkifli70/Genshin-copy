afe6942942b93175881e2808fa3611114938fa84
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-24 15:07:07 +0700

    complete footer and all its content

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -13,36 +13,36 @@ import {
 const Footer = () => {
   return (
     <footer>
-      <div className="flex gap-15 justify-center py-4 bg-[#1a1a1aee] border-b-1 border-b-gray-500">
+      <div className="flex gap-15 justify-center py-4 bg-black border-b-[0.5px] border-b-gray-500">
         <span>
-          <FaXTwitter color="gray" size={35} />
+          <FaXTwitter className="soc-icon" size={35} />
         </span>
         <span>
-          <FaTiktok color="gray" size={35} />
+          <FaTiktok className="soc-icon" size={35} />
         </span>
         <span>
-          <FaYoutube color="gray" size={35} />
+          <FaYoutube className="soc-icon" size={35} />
         </span>
         <span>
-          <FaFacebookF color="gray" size={35} />
+          <FaFacebookF className="soc-icon" size={35} />
         </span>
         <span>
-          <FaInstagram color="gray" size={35} />
+          <FaInstagram className="soc-icon" size={35} />
         </span>
         <span>
-          <FaDiscord color="gray" size={35} />
+          <FaDiscord className="soc-icon" size={35} />
         </span>
         <span>
-          <FaRedditAlien color="gray" size={35} />
+          <FaRedditAlien className="soc-icon" size={35} />
         </span>
         <span>
-          <FaTwitch color="gray" size={35} />
+          <FaTwitch className="soc-icon" size={35} />
         </span>
         <span>
-          <FaFacebookMessenger color="gray" size={35} />
+          <FaFacebookMessenger className="soc-icon" size={35} />
         </span>
         <span>
-          <FaWhatsapp color="gray" size={35} />
+          <FaWhatsapp className="soc-icon" size={35} />
         </span>
       </div>
       <div className="flex px-40 py-15 bg-[#212226] text-white">
@@ -56,21 +56,80 @@ const Footer = () => {
             event rewards.
           </p>
         </div>
-        <div className="flex-1 flex items-center justify-center text-2xl">
+        <div className="flex-1 flex items-center justify-end text-xl">
           <form>
             <input
               name="email"
               placeholder="Email"
-              className="bg-[#212226] rounded-md px-2 font-light py-3 mr-4 border-1 border-[#323339]"
+              className="bg-[#212226] w-md rounded-xl px-2 font-light py-3 mr-4 border-1 border-[#323339]"
             />
             <button
               type="submit"
-              className="px-6 py-3 rounded-md bg-[#cdbb98] text-black font-light"
+              className="px-6 py-3 rounded-xl bg-[#cdbb98] text-black font-light"
             >
               Subscribe Now
             </button>
           </form>
         </div>
+      </div>
+      <div className="bg-black flex flex-col items-center justify-center p-4 text-gray-300 gap-8">
+        <img
+          src="/genshin-logo.png"
+          alt="genshin logo footer"
+          className="w-70"
+        />
+        <div className="flex gap-6">
+          <a
+            href="#"
+            className="footer-link
+          "
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="footer-link
+          "
+          >
+            Term of Service
+          </a>
+          <a
+            href="#"
+            className="footer-link
+          "
+          >
+            About Us
+          </a>
+          <a
+            href="#"
+            className="footer-link
+          "
+          >
+            Contact Us
+          </a>
+          <a
+            href="#"
+            className="footer-link
+          "
+          >
+            Help Center
+          </a>
+        </div>
+        <img src="/teen.png" alt="" />
+        <div className="text-center">
+          <p>
+            "PlayStation Family Mark" and "PS5 logo" are registered trademarks
+            or trademarks of Sony Interactive Entertainment Inc.
+          </p>
+          <p className="w-5xl">
+            Epic, Epic Games, Epic Games Store, the Epic Games Store logo, and
+            Epic Online Services are trademarks and/or registered trademarks of
+            Epic Games. All other trademarks are the property of their
+            respective owners.
+          </p>
+        </div>
+        <img src="/hoyoverse.png" alt="hoyoverse logo" className="w-100" />
+        <p>Copyright © COGNOSPHERE. All Rights Reserved.</p>
       </div>
     </footer>
   );
